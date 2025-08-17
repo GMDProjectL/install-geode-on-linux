ea03871123d90c4d8cb23fc1e63ce7ecdb58b144
Andrew <kezin636@gmail.com>
2025-08-17 22:02:16 +0300

    installation instructions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install Geode on Linux
+
+## Run using `wget`:
+```bash
+wget -O- https://github.com/GMDProjectL/install-geode-on-linux/raw/refs/heads/main/run.sh | bash
+```
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/GMDProjectL/install-geode-on-linux/releases/download/binary/installer -O /tmp/install-geode
+chmod +x /tmp/install-geode
+/tmp/install-geode
